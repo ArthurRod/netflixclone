111bab2c1f547eb0d975e61316c492b482ca07f4
ArthurRod <arthur9713@hotmail.com>
2022-03-10 18:19:02 -0300

    Sliders de filmes adicionados

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Tmdb from "./Tmdb";
+import './App.css';
+import MovieRow from "./components/MovieRow";
+import FeaturedMovie from "./components/FeaturedMovie";
 
-export default() => {
-  return(
-    <div>Olá mundo</div>
-  )
-}
+export default () => {
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      // Pega a lista total
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      // Pegando o Featured
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+      let chosen = originals[0].items.results[randomChosen]
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
+
+      console.log(chosenInfo)
+      setFeaturedData(chosenInfo)
+    };
+
+    loadAll();
+  }, []);
+
+  return (
+    <div className="page">
+
+      {featuredData && 
+        <FeaturedMovie item={featuredData} />
+      }
+
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+    </div>
+  );
+};
